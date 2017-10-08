34fb7a4023f33767286b58043cb7aa3c34b473f8
Kari Lindberg <karisoolindberg@gmail.com>
2017-10-08 20:24:30 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function shout(string)  {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+  function logShout(string) {
+    console.log(string.toUpperCase())
+  }
+  function logWhisper(string){
+    console.log(string.toLowerCase())
+  }
+
+function sayHiToGrandma(string){
+    var string2 = "I can't hear you!"
+  if(string = string.toLowerCase){
+    console.log(string2)
+    return string2
+  }
+}
+
+function sayHiToGrandma(string){
+  var string3 = "YES INDEED!"
+  if(string = string.toUpperCase){
+    console.log(string3)
+    return string3
+  }
+}
